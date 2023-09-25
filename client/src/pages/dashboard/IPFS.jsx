2de60220310f164188e45f6b7294fa7e52783483
Siddheshsingh <siddheshsinghtawar@gmail.com>
2023-09-25 20:29:35 +0530

    Minor changes

---
+++ client/src/pages/dashboard/IPFS.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react"; // Import React and useState
+import { Web3Storage } from "web3.storage";
+
+const IPFS = () => {
+  return <dive></dive>;
+};
+export default IPFS;
